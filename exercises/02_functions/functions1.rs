228fa32e794e0ff70dfec7a4dea376bcc4734cb1
TheJieee <patrick.jieee@gmail.com>
2024-12-25 23:28:57 +0800

    4-1

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -1,4 +1,7 @@
 // TODO: Add some function with the name `call_me` without arguments or a return value.
+fn call_me() {
+    //do nothing.
+}
 
 fn main() {
     call_me(); // Don't change this line
